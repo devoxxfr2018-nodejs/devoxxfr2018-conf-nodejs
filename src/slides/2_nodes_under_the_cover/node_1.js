b2e5da3aab2c13d64f4c08c636ea7ef783d596b1
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-03-18 16:47:16 +0100

    init

---
+++ src/slides/2_nodes_under_the_cover/node_1.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Heading, Notes, Slide, Image, Text } from "spectacle";
+
+export default (
+    <Slide transition={['zoom']} bgColor="primary">
+        <Image src="assets/speccollab2.gif" width="90%" />
+        <Text>
+            TODO
+            TODO
+        </Text>
+        <Notes>
+            
+            
+        </Notes>
+    </Slide>
+
+);
