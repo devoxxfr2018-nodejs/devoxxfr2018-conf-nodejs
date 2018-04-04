8e35a2e7f14056bd8519da2139f775e0f6270d22
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-04-04 12:58:07 +0200

    Présentation des speakers

--- src/slides/0_intro/intro_2.js
+++ src/slides/0_intro/intro_2.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Heading, Slide, Text, Notes, List, ListItem } from "spectacle";
+import { Heading, Slide, Text, Notes, List, ListItem, Layout, Fill, Image } from "spectacle";
 
 export default (
-    <Slide  >
-     <Heading>TODO SPEAKERS</Heading>
-     <List>
-        <ListItem>Alexandre Victoor</ListItem>
-        <ListItem>alexandre.victoor@fluo.com</ListItem>
-        <ListItem>@alex_victoor</ListItem>
-        <ListItem>https://github.com/alexvictoor</ListItem>
-    </List>
+    <Slide>
+        <Heading margin="0px 0px 50px 0px">NOUS</Heading>
+     <Layout>
+         <Fill>
+             <Image src="assets/fluo_diag.png" />
+             
+         </Fill>
+         <Fill>
+            <Heading size={4} textAlign="left">Alexandre Victoor</Heading>
+            <Text textAlign="left" margin="30px">@Alex_Victoor</Text>
+            <Text textAlign="left" margin="30px">github.com/alexvictoor</Text>
+            <Heading size={4} textAlign="left">Thierry Abaléa</Heading>
+            <Text textAlign="left" margin="30px">@ThierryAbalea ‏</Text>
+            <Text textAlign="left" margin="30px">github.com/ThierryAbalea</Text>
+            
+         </Fill>
+    </Layout>
+    <Image src="assets/fluo_logo.png" margin="40px 40 px" />
      <Notes>
      
     </Notes>
