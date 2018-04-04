88544dff1f78a03b933758ed4202b96cbe5f6ede
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-04-04 13:55:18 +0200

    fluo love js

--- src/slides/0_intro/intro_4.js
+++ src/slides/0_intro/intro_4.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Heading, Notes, Slide } from "spectacle";
+import { Heading, Notes, Slide, Layout, Image, Text } from "spectacle";
 
 export default (
-    <Slide bgColor="primary"  bgImage="assets/say_javascript_athletic_heather_front.jpg" >
-        <Heading>FLUO love JS</Heading>
-   
+    <Slide >
+      
+        <Layout margin="-200px">
+            <Image  src="assets/fluo_logo.png" width="30%" height="30%"/>
+            <Text textSize="150"><span role="img" aria-label="love">❤️</span></Text>
+            <Image src="assets/js-logo.svg" />
+        </Layout>
+
         <Notes>
         </Notes>
     </Slide>
