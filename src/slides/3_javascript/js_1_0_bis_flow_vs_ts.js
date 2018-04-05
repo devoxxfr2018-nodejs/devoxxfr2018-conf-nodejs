43a634bf2882403aba0a2cd5ceb0f05289f820b7
Thierry ABALEA <thierry.abalea@fluo.com>
2018-04-05 12:49:22 +0200

    add missing files

---
+++ src/slides/3_javascript/js_1_0_bis_flow_vs_ts.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Heading, Notes, Slide, Image } from "spectacle";
+
+export default (
+    <Slide>
+        <Image src="assets/flow-vs-typescript.png" />
+    </Slide>
+);
