f95fc1346f7315ac365c20cbd5b4a277d55ab405
Thierry ABALEA <thierry.abalea@fluo.com>
2018-04-05 12:57:59 +0200

    add slides about react js/native/ssr

---
+++ src/slides/4_ecosystem/eco_5c_react_ssr.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Heading, Notes, Slide, Image, Text } from "spectacle";
+
+export default (
+  <Slide bgColor="primary">
+      <Image src="assets/react-ssr.png" width="80%" height="80%" margin="30" />
+  </Slide>
+);
