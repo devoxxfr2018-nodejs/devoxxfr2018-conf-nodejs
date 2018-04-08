c3d8e2a4afc49c17468546f37ea58466b9d4e1fd
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-04-08 22:49:27 +0200

    ssr resize

--- src/slides/4_ecosystem/eco_5c_react_ssr.js
+++ src/slides/4_ecosystem/eco_5c_react_ssr.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Heading, Notes, Slide, Image, Text } from "spectacle";
 
 export default (
-  <Slide bgColor="primary">
-      <Image src="assets/react-ssr.png" width="80%" height="80%" margin="30" />
+  <Slide bgColor="primary" bgImage="assets/react-ssr.png" bgSize="90%" bgRepeat="no-repeat">
   </Slide>
 );
