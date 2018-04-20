d5c81a284c3cb85340075dc0fa52cd3b2ede2b37
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-04-20 12:08:42 +0200

    last minute

--- src/slides/3_javascript/js_1_3_sandbox.js
+++ src/slides/3_javascript/js_1_3_sandbox.js
@@ -5,25 +5,8 @@ import CodeSlide from 'spectacle-code-slide';
 import code from './hello.nest.example';
 
 export default (
-  <Slide transition={['spin']} >
-          <iframe 
-            title="TypeScript FTW"
-            src="https://codesandbox.io/embed/725n32j8jx?autoresize=1&expanddevtools=1&fontsize=42&hidenavigation=1&module=%2Fsrc%2Fhello-typescript.ts&view=editor" 
-            style={ {
-              position: "absolute",
-              left: "0",
-              right: "0",
-              top: "0",
-              bottom: "0",
-              height: "900px",
-              width: "150%",
-              marginTop: "-40%",
-              marginBottom: "-30%",
-              marginLeft: "-25%",
-             
-          } } 
-            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin">
-          </iframe>
+  <Slide transition={['spin']} bgImage="assets/codesandbox.png" bgSize="50%" bgRepeat="no-repeat" bgColor="black">
+     
       <Notes>
         <ul>
           <li>annotations - decorateurs</li>
